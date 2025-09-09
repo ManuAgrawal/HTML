e3263b9763abb64b471f12d6ba7e68804f28b7ec
Manu Agrawal <agrawalmanu73@gmail.com>
2025-09-09 10:23:55 +0530

    new

---
+++ JS Index/day_4.js
@@ -0,0 +1,17 @@
+// console.log(document);
+
+//  let h1   =   document.getElementById("one")
+// h1.style.color = "red"
+//  console.log(h1);
+ 
+
+// let h1 = document.getElementsByClassName("two")
+// h1[1].style.color= 'blue';
+// console.log(h1);
+
+// let h1 = document.querySelector(".two")
+// console.log(h1);
+
+// let h1 = document.querySelectorAll(".two")
+// console.log(h1);
+
